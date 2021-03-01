b68d534fd76cc065acacea913d1262a7e8efbc23
aislanacf <aislanacf@gmail.com>
2021-03-01 16:42:43 -0300

    adicionados atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,13 @@
 class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá {self.nome}'
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
+    p = Pessoa('Fulano',43)
+    #print(Pessoa.cumprimentar(p))
+    #print(id(p))
     print(p.cumprimentar())
